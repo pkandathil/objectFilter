d0c8c9e74f4003d5631f574f1e5e368e49063916
pkandathil <prashantgeorge36@gmail.com>
2019-05-03 14:13:41 -0700

    Initial commit with failing test for complex object

---
+++ app.js
@@ -0,0 +1,55 @@
+const objectFilter = (inputObject, filter) => {
+  //Can improve to catch more negative test cases
+  if (!inputObject) {
+    throw new Error('Missing input object')
+  }
+  if (!filter) {
+    throw new Error('Missing filter')
+  }
+  const filterParsed = filter.split('=')
+  const filterFieldNames = filterParsed[0].split('.')
+  let filterValues = filterParsed[1].split(',')
+
+  //assumed filtering only on int value
+  filterValues = filterValues.map((value) => {
+    return parseInt(value)
+  })
+
+  //Traversing to field
+  let objectToFilter = inputObject
+  let filterFieldName = filterFieldNames[filterFieldNames.length-1]
+  filterFieldNames.forEach((fieldName, index) => {
+    if(index !== filterFieldNames.length - 1 && filterFieldNames.length > 1) {
+      objectToFilter = objectToFilter[fieldName]
+    }
+  })
+
+  //Filtering values
+  const filteredResult = objectToFilter.filter((item) => {
+    let shouldKeepItem = false
+    const currentFieldValue = item[filterFieldName]
+    const indexOfItem = filterValues.indexOf(currentFieldValue)
+    if (indexOfItem > -1) {
+      shouldKeepItem = true
+    }
+    return shouldKeepItem
+  })
+
+  //Setting result
+  let result = {...inputObject}
+  if(filterFieldNames.length === 1) {
+    result = filteredResult
+  } else {
+    filterFieldNames.forEach((fieldName, index) => {
+      if(index === filterFieldNames.length - 2) {
+        result[fieldName] = filteredResult
+      }
+    })
+  }
+  
+  return result
+}
+
+module.exports = {
+  objectFilter
+}
---
+++ test/app.spec.js
@@ -0,0 +1,136 @@
+const { objectFilter } = require('../app')
+
+describe('Object Filtering', () => {
+  beforeEach(() => {
+  });
+
+  afterEach(() => {
+  });
+
+  it('throw error for missing input object', () => {
+    const filter = 'asdf'
+    try {
+      objectFilter(null, filter)
+    } catch (error) {
+      expect(error.message).toEqual('Missing input object')
+    }
+  })
+
+  it('throw error for missing filter', () => {
+    const inputObject = 'asdf'
+    try {
+      objectFilter(inputObject, null)
+    } catch (error) {
+      expect(error.message).toEqual('Missing filter')
+    }
+  })
+
+  it('filter items in array by field value', () => {
+    const inputObject = [
+      {
+        value: 1
+      },
+      {
+        value: 2
+      },
+      {
+        value: 3
+      },
+      {
+        value: 4
+      }
+    ]
+    const filter = 'value=1,3'
+    const result = objectFilter(inputObject, filter)
+    expect(result.length).toEqual(2)
+  })
+
+  it('filter items in object with array by field value', () => {
+    const inputObject = {
+        items: [
+        {
+          value: 1
+        },
+        {
+          value: 2
+        },
+        {
+          value: 3
+        },
+        {
+          value: 4
+        }
+      ]
+    }
+    const filter = 'items.value=1,3'
+    const result = objectFilter(inputObject, filter)
+    expect(result.items.length).toEqual(2)
+  })
+
+  it.only('filter items in more complex object with array by field value', () => {
+    const inputObject = {
+      "ShoppingCart": {
+        "id": "123-456-789",
+        "characteristic": [
+          {
+            "name": "123456",
+            "value": null,
+            "displayName": "Test char 1",
+            "originalDisplayName": "Test char 1",
+            "description": "Test characteristic object 1"
+          },
+          {
+            "name": "78910",
+            "value": null,
+            "displayName": "Test char 2",
+            "originalDisplayName": "Test char 2",
+            "description": "Test characteristic object 2"
+          }
+        ],
+        "cartItem": [
+          {
+            "action": "Add",
+            "id": "1",
+            "product": {
+              "id": null,
+              "displayName": "Test product 1",
+              "originalName": "Test product 1",
+              "characteristic": [
+                {
+                  "name": "Testing 1",
+                  "value": "12"
+                },
+                {
+                  "name": "Testing 2",
+                  "value": "34"
+                }
+              ]
+            }
+          },
+          {
+            "action": "Add",
+            "id": "2",
+            "product": {
+              "id": null,
+              "displayName": "Test product 2",
+              "originalName": "Test product 2",
+              "characteristic": [
+                {
+                  "name": "Testing 3",
+                  "value": "56"
+                },
+                {
+                  "name": "Testing 4",
+                  "value": "78"
+                }
+              ]
+            }
+          }
+        ]
+      }
+    }
+    const filter = 'ShoppingCart.cartItem.product.characteristic.value=34,56'
+    const result = objectFilter(inputObject, filter)
+    expect(result.ShoppingCart.cartItem.product.characteristic.length).toEqual(2)
+  }) 
+})
