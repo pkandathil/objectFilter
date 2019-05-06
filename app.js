cc6c384d696dd1871a9dd256f324dae1f4c5d3cb
pkandathil <prashantgeorge36@gmail.com>
2019-05-06 08:36:26 -0700

    Added in ramda for cloning and a little more error checking

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const { clone } = require('ramda')
+
 const objectFilter = (inputObject, filter) => {
   //Can improve to catch more negative test cases
   if (!inputObject) {
@@ -10,25 +12,26 @@ const objectFilter = (inputObject, filter) => {
   const filterFieldNames = filterParsed[0].split('.')
   let filterValues = filterParsed[1].split(',')
 
-  let objectToFilter = JSON.parse(JSON.stringify(inputObject));
+  let objectToFilter = clone(inputObject)
 
   if (filterFieldNames.length > 1) {
-    
+    //Recursion
     if(Array.isArray(objectToFilter)) {
       //Looping over array items
       objectToFilter = objectToFilter.map((item) => {
         return objectFilter(item, filter)
       })
     } else {
-      //Recursion
       const nextObject = objectToFilter[filterFieldNames[0]]
       const newFilter = `${filterFieldNames.slice(1).join('.')}=${filterParsed[1]}`
       objectToFilter[filterFieldNames[0]] = objectFilter(nextObject, newFilter)
     }
     return objectToFilter
   } else {
-    //Stopping point for reccursion
     let filterFieldName = filterFieldNames[filterFieldNames.length-1]
+    if(!Array.isArray(objectToFilter)) {
+      throw new Error('Invalid filter')
+    }
     //Filtering values
     const filteredResult = objectToFilter.filter((item) => {
       let shouldKeepItem = false
